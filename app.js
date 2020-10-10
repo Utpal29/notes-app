30d2ac0c2c009968ad1a94881a3d884a0f9f77e7
Utpal29 <utpal29flash@gmail.com>
2020-10-11 00:04:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
+
+yargs.version('1.1.0');
+
+// Create add comand
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log(`Title: ${argv.title}`);
+    console.log(`Body: ${argv.body}`);
+  }
+});
+
+// Create remove comand
+yargs.command({
+  command: 'remove',
+  describe: "Remove a note",
+  handler: function () {
+    console.log('removeing a note');
+  }
+});
+
+// Create list comand
+yargs.command({
+  command: 'list',
+  describe: "list your notes",
+  handler: function () {
+    console.log('listing notes');
+  }
+});
+
+// Create read comand
+yargs.command({
+  command: 'read',
+  describe: "read a note",
+  handler: function () {
+    console.log('reading a note');
+  }
+});
+
+yargs.parse();
+// console.log(yargs.argv);
